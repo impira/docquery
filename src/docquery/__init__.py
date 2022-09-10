135d0638cc48564ee7b898c9752576f6043f6574
Ankur Goyal <ankur@impira.com>
2022-09-10 13:54:11 -0700

    Lazily import pipeline

--- src/docquery/__init__.py
+++ src/docquery/__init__.py
@@ -1,3 +1,20 @@
 # pipeline wraps transformers pipeline with extensions in DocQuery
 # we're simply re-exporting it here.
-from .transformers_patch import pipeline  # noqa
+import sys
+
+from transformers.utils import _LazyModule
+
+from .version import VERSION
+
+
+_import_structure = {
+    "transformers_patch": ["pipeline"],
+}
+
+sys.modules[__name__] = _LazyModule(
+    __name__,
+    globals()["__file__"],
+    _import_structure,
+    module_spec=__spec__,
+    extra_objects={"__version__": VERSION},
+)
